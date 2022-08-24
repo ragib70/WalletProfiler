6192e6a1ab894a3051d49d0d199b4b12b47aac22
ragib70 <ragib.hussain70@gmail.com>
2022-08-24 22:54:27 +0530

    Adding tablewith dynamic depth

--- wallet-profiler-dune/src/App.tsx
+++ wallet-profiler-dune/src/App.tsx
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Card from "./components/Card/Card";
+import Table from "./components/Table/Table";
 
 function App() {
   return (
     <div className="">
       <NavBar />
       <Card />
+      <Table />
     </div>
   );
 }
--- wallet-profiler-dune/src/components/Card/Card.tsx
+++ wallet-profiler-dune/src/components/Card/Card.tsx
@@ -3,17 +3,20 @@ import { FC, useState } from "react";
 interface CardProps {}
 
 const Card: FC<CardProps> = (props) => {
+  const [value, setValue] = useState(160000); // Get this predefined value by calling a function.
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="card w-auto p-5" style={{ width: "18rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <h5 className="card-title">Estimated Wallet Value</h5>
+        <p className="card-text">${value}</p>
+        <button
+          className="btn btn-outline-success me-3"
+          onClick={() => {
+            // Do  Loading of Tables
+          }}
+        >
+          Load
+        </button>
       </div>
     </div>
   );
