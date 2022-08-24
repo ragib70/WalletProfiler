29c69cda00bc7b81e3396ace024e2178fb3837ea
ragib70 <ragib.hussain70@gmail.com>
2022-08-24 17:42:33 +0530

    Login Functionality added + Card added

--- wallet-profiler-dune/src/App.tsx
+++ wallet-profiler-dune/src/App.tsx
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Card from "./components/Card/Card";
 
-function App(){
+function App() {
   return (
     <div className="">
-        <NavBar />
+      <NavBar />
+      <Card />
     </div>
   );
 }
---
+++ wallet-profiler-dune/src/components/Card/Card.tsx
@@ -0,0 +1,22 @@
+import { FC, useState } from "react";
+
+interface CardProps {}
+
+const Card: FC<CardProps> = (props) => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
